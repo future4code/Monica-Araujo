042212447347c4b9c4af2670ba35ece47de7a001
Monica-Araujo <monicaaraujo974@gmail.com>
2021-08-23 18:11:49 -0300

    Exercício 1 finalizado

---
+++ semana17/aula49-filtros/filtros-ordenaçoes/template-filtros-ordenacao-paginacao/src/index.ts
@@ -0,0 +1,25 @@
+import express from "express"
+import cors from "cors"
+import { AddressInfo } from "net"
+import { getAllUsers } from "./endpoints/getAllUsers"
+import { getUsersByName } from "./endpoints/getUsersByName"
+import { getUsersByType } from "./endpoints/getUsersByType"
+
+export const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/allUsers", getAllUsers)
+app.get("/users/name", getUsersByName)
+app.get("/users/:type", getUsersByType)
+
+
+const server = app.listen(process.env.PORT || 3003, () => {
+   if (server) {
+      const address = server.address() as AddressInfo;
+      console.log(`Server is running in http://localhost:${address.port}`);
+   } else {
+      console.error(`Failure upon starting server.`);
+   }
+})
