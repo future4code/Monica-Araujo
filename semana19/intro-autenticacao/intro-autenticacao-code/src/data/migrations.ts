f5df8434a4c14299872493cf39426cf522456088
Monica-Araujo <monicaaraujo974@gmail.com>
2021-09-07 19:03:09 -0300

    iniciando as atividades

---
+++ semana19/intro-autenticacao/intro-autenticacao-code/src/data/migrations.ts
@@ -0,0 +1,15 @@
+import { connection } from "./connection";
+
+connection.raw(`
+   CREATE TABLE User (
+      id VARCHAR(255) PRIMARY KEY,
+      email VARCHAR(255) UNIQUE NOT NULL,
+      password VARCHAR(255) NOT NULL
+   );
+`).then(() => {
+   console.log("Tabelas criadas!");
+}).catch(error => {
+   console.log(error.sqlMessage || error.message);
+}).finally(()=>{
+   connection.destroy()
+})
